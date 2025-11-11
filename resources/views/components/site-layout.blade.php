3b16dc3bf9087aa7c474a3aec933b4911f0c4c70
Nico Deblauwe <nico@bluepundit.eu>
2025-11-11 15:55:59 +0100

    DESIGN :art: Add a site layout

---
+++ resources/views/components/site-layout.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+<body class="white">
+<header class="bg-teal-500 text-teal-50 mb-2">
+    <div class="mx-auto w-2/3">
+        menu
+    </div>
+</header>
+<main>
+    <div class="mx-auto w-2/3">
+        {{$slot}}
+    </div>
+</main>
+<footer class="bg-teal-100 mt-10 h-20">
+    <div class="mx-auto w-2/3">
+        some really important legal stuff
+    </div>
+
+</footer>
+</body>
+</html>
+
+
