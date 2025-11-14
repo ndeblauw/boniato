6e5feb8b8a70f5ff5a3047c2d0c1f738c54c9f86
Nico Deblauwe <nico@bluepundit.eu>
2025-11-14 14:15:31 +0100

    REFACTOR :hammer: Switch to resourceful route definition

--- routes/web.php
+++ routes/web.php
@@ -7,17 +7,7 @@ Route::get('/', [\App\Http\Controllers\WelcomeController::class, 'index']);
 Route::get('articles', [\App\Http\Controllers\ArticleController::class, 'index'])->name('articles.index');
 Route::get('articles/{id}', [\App\Http\Controllers\ArticleController::class, 'show'])->name('articles.show');
 
-Route::get('categories/create', [\App\Http\Controllers\CategoryController::class, 'create'])->name('categories.create');
-Route::post('categories', [\App\Http\Controllers\CategoryController::class, 'store'])->name('categories.store');
-Route::get('categories/{id}/edit', [\App\Http\Controllers\CategoryController::class, 'edit'])->name('categories.edit');
-Route::put('categories/{id}', [\App\Http\Controllers\CategoryController::class, 'update'])->name('categories.update');
-
-Route::delete('categories/{id}', [\App\Http\Controllers\CategoryController::class, 'destroy'])->name('categories.destroy');
-
-Route::get('categories', [\App\Http\Controllers\CategoryController::class, 'index'])->name('categories.index');
-Route::get('categories/{id}', [\App\Http\Controllers\CategoryController::class, 'show'])->name('categories.show');
-
-
+Route::resource('categories', \App\Http\Controllers\CategoryController::class);
 
 Route::get('/dashboard', function () {
     return view('userzone.dashboard');
