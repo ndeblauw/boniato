614c06c1e1912c341d1eb7f9955efa9037d49296
Nico Deblauwe <nico@bluepundit.eu>
2025-11-21 16:42:44 +0100

    DESIGN :art: Also display author info on the index

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -7,10 +7,14 @@
                     <img class="w-full" src="{{$article->getImageUrl('preview')}}" alt="article main image">
                 </div>
                 <div>
-                    <div class="">
-                        @foreach($article->categories as $category)
-                            <a href="/categories/{{$category->id}}" class="bg-[#FC6E7F] text-[#26054D] rounded-full px-2">{{$category->name}}</a>
-                        @endforeach
+                    <div class="flex items-center gap-x-6 mb-4 -mt-2">
+                        <div class="">
+                            @foreach($article->categories as $category)
+                                <a href="/categories/{{$category->id}}" class="bg-[#FC6E7F] text-[#26054D] rounded-full px-2">{{$category->name}}</a>
+                            @endforeach
+                        </div>
+                        |
+                        <div class="text-gray-700">written by <span class="font-semibold">{{$article->author->name}}</span></div>
                     </div>
                     <a href="/articles/{{$article->id}}" class="block mb-2 hover:gray-50">
                         <h2 class="font-bold text-xl">{{$article->title}}</h2>
