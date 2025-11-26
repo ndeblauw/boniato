fcf860aee28ab3979afc887a61b3b0dddef816ab
Nico Deblauwe <nico@bluepundit.eu>
2025-11-26 16:35:03 +0100

    DESIGN :art: Make frontpage clickable

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,10 +20,9 @@
 
                 <h1 class="text-4xl font-bold mb-4">{{$article->title}}</h1>
 
-                <div>
+                <a href="/articles/{{$article->slug}}" class="line-clamp-5">
                     {{$article->content}}
-
-                </div>
+                </a>
 
             </div>
         </div>
@@ -34,6 +33,7 @@
     <div class="grid grid-cols-3 gap-12">
         @foreach($articles as $article)
             <div class="bg-linear-to-br from-[#7B7075]/5 via-orange-100 to-[#7B7075]/10 p-1 mb-8 rounded">
+
                 <img class="w-full" src="{{$article->getImageUrl('website')}}" alt="article main image">
 
                 <div class="p-4">
@@ -47,10 +47,9 @@
 
                     <h1 class="text-2xl font-bold mb-4 line-clamp-2">{{$article->title}}</h1>
 
-                    <div class="line-clamp-5">
+                    <a href="/articles/{{$article->slug}}" class="line-clamp-5">
                         {{$article->content}}
-
-                    </div>
+                    </a>
 
                 </div>
 
