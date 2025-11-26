89958a92d6f5de628cb4a67f8580fc39e6edec08
Nico Deblauwe <nico@bluepundit.eu>
2025-11-26 13:33:35 +0100

    FEATURE :sparkles: Make cache ttl a parameter

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -9,7 +9,7 @@ class WelcomeController extends Controller
     function __invoke()
     {
         // Load 4 most recent articles
-        $articles = cache()->remember('welcome_page_articles', 3600, function() {
+        $articles = cache()->remember('welcome_page_articles', config('app.cache_ttl'), function() {
             return \App\Models\Article::query()
                 ->with('author:id,name', 'media', 'categories')
                 ->orderBy('created_at', 'desc')
