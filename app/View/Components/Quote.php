3cc5dc01827f7df6161c1a40ebc9430198bcefb0
Nico Deblauwe <nico@bluepundit.eu>
2026-01-15 16:09:55 +0100

    REFACTOR :hammer: Improve the API usagage

--- app/View/Components/Quote.php
+++ app/View/Components/Quote.php
@@ -22,24 +22,41 @@ class Quote extends Component
      */
     public function render(): View|Closure|string
     {
+        $quote = cache()->flexible('random-quote', [10, 20], function() {
+            ray('cache miss: fetching new quote')->orange();
+            return $this->fetchQuote();
+        });
+
+        return view('components.quote', compact('quote'));
+    }
+
+    private function fetchQuote()
+    {
         $endpoint="https://api.api-ninjas.com/v2/randomquotes";
         $key = config('services.api_ninjas.key');
 
-        $response = Http::withHeaders([
-            'X-Api-Key' => $key,
-        ])->get($endpoint);
+        try {
+            ray('start fetching quote')->green();
+            sleep(3);
 
-        if($response->failed()) {
+            $response = Http::withHeaders([
+                'X-Api-Key' => $key,
+            ])->get($endpoint);
+
+            if($response->successful()) {
+                $quote = json_decode($response->body())[0];
+            } else {
+                throw new \Exception('API request failed');
+            }
+        } Catch (\Exception $e) {
             $quote = (object) [
                 'quote' => 'Don\'t worry about the presentation tomorrow.',
                 'author' => 'Nico Deblauwe',
             ];
-        } else {
-            $quote = json_decode($response->body())[0];
-
         }
 
+        ray('finished fetching quote')->green();
 
-        return view('components.quote', compact('quote'));
+        return $quote;
     }
 }
