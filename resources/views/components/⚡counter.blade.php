c0674654cd5a10212291f1d95c10bea63f4054c0
Nico Deblauwe <nico@bluepundit.eu>
2026-01-20 10:34:58 +0100

    REFACTOR :hammer: Use $get for counter to simplify

--- resources/views/components/⚡counter.blade.php
+++ resources/views/components/⚡counter.blade.php
@@ -5,22 +5,11 @@ use Livewire\Component;
 new class extends Component
 {
     public int $counter;
-
-    public function increment()
-    {
-        $this->counter++;
-    }
-
-    public function decrement()
-    {
-        $this->counter--;
-    }
-
 };
 ?>
 
 <div class="p-4 bg-red-100 text-red-800 rounded-lg text-center">
-    <button wire:click="decrement()" class="p-2 bg-red-200">-</button>
+    <button wire:click="$set('counter', {{$counter-1}})" class="p-2 bg-red-200">-</button>
     <span>{{$counter}}</span>
-    <button wire:click="increment()" class="p-2 bg-red-200">+</button>
+    <button wire:click="$set('counter', {{$counter+1}})" class="p-2 bg-red-200">+</button>
 </div>
