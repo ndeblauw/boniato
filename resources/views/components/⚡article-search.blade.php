cde5b2a53be15670d4d2b26992603ffaf78fe6bf
Nico Deblauwe <nico@bluepundit.eu>
2026-01-20 10:08:49 +0100

    FEATURE :sparkles: Livewire implementation of search

--- resources/views/components/⚡article-search.blade.php
+++ resources/views/components/⚡article-search.blade.php
@@ -6,10 +6,16 @@ use Livewire\Component;
 new class extends Component {
     public $articles = [];
     public string $search = '';
+    public ?int $category_id = null;
 
     public function updatedSearch()
     {
         $this->articles = Article::where('title', 'like', '%' . $this->search . '%')
+            ->when($this->category_id, function ($query) {
+                $query->whereHas('categories', function ($query) {
+                    $query->where('id', $this->category_id);
+                });
+            })
             ->orderBy('title')
             ->take(10)
             ->get();
@@ -22,10 +28,18 @@ new class extends Component {
 
     <input wire:model.live="search" type="text" placeholder="Search articles..." class="border p-2 w-full"/>
 
+    <select wire:model.live="category_id" label="Category">
+        <option value="">-- All Categories --</option>
+        @foreach(App\Models\Category::orderBy('name')->pluck('name','id') as $id => $name)
+            <option value="{{ $id }}">{{ $name }}</option>
+        @endforeach
+    </select>
+
+
     <div class="text-sky-600">
         {{$search}}
-
     </div>
+
 
     @foreach($articles as $article)
         <div class="mt-2">
